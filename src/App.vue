14121b79a1f181b55c12afcddc465eed5dabca1b
Maikol-08 <mbadilloagredo@gmail.com>
2025-12-04 17:42:34 -0500

    segundo avance examen_final

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,12 @@
 <template>
-  <div>
-    <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
-      <div class="container-fluid">
-        <a class="navbar-brand" href="#">Mi Tienda</a>
-        <div class="d-flex">
-          <router-link to="/products" class="btn btn-outline-light me-2">
-            <i class="bi bi-list"></i> Productos
-          </router-link>
-          <router-link to="/products/new" class="btn btn-outline-light">
-            <i class="bi bi-plus-lg"></i> Nuevo
-          </router-link>
-        </div>
-      </div>
-    </nav>
-
-    <main class="container my-4">
-      <router-view />
-    </main>
+  <div id="app">
+    <!-- Aquí se renderiza la vista activa -->
+    <router-view />
   </div>
 </template>
 
 <script>
-export default { name: 'App' }
+export default {
+  name: 'App'
+}
 </script>
-
-<style>
-body { background: #f7f9fc; }
-</style>
--- src/services/productService.js
+++ src/services/productService.js
@@ -1,14 +1,26 @@
 import axios from 'axios'
 
-const api = axios.create({
-  baseURL: 'https://691d0016d58e64bf0d34ab72.mockapi.io/api/v1',
-  timeout: 10000
-})
+const BASE_URL = 'https://691d0016d58e64bf0d34ab72.mockapi.io/api/v1'
+const RESOURCE = `${BASE_URL}/products`
 
 export default {
-  async getAll() { return (await api.get('/products')).data },
-  async create(product) { return (await api.post('/products', product)).data },
-  async getById(id) { return (await api.get(`/products/${id}`)).data },
-  async update(id, product) { return (await api.put(`/products/${id}`, product)).data },
-  async remove(id) { return (await api.delete(`/products/${id}`)).data }
+  getAllProducts() {
+    return axios.get(RESOURCE)
+  },
+
+  getProductById(id) {
+    return axios.get(`${RESOURCE}/${id}`)
+  },
+
+  createProduct(data) {
+    return axios.post(RESOURCE, data)
+  },
+
+  updateProduct(id, data) {
+    return axios.put(`${RESOURCE}/${id}`, data)
+  },
+
+  deleteProduct(id) {
+    return axios.delete(`${RESOURCE}/${id}`)
+  }
 }
